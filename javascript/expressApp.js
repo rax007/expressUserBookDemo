d8363b27a2b770716ce68e4c81ee5d3fb389f45e
rakesh <rakesh.kumar@gmail.com>
2014-07-10 15:53:21 +0530

    post rest api done

--- javascript/expressApp.js
+++ javascript/expressApp.js
@@ -3,27 +3,29 @@
  */
 
 var express = require('express'),
-    util = require('./util'),
     admins = require('./admins'),
     users = require('./users'),
-    books = require('./books');
+    books = require('./books'),
+    bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser());
 
+//Get all user list
 app.get('/user/:id?', function(req, res) {
 
     users.getUserDetails(function (errr, result) {
 
         if(!req.param('id'))
         {
-            res.send(result);
+            res.send(200,result);
         }
         else
         {
             var id = req.param('id');
             for (var obj in result) {
                 if(result[obj]._id == id)
-                res.send(result[obj]);
+                res.send(200, result[obj]);
             }
         }
 
@@ -32,26 +34,49 @@ app.get('/user/:id?', function(req, res) {
 
 });
 
-
+//Get all admin list
 app.get('/admin/:id?', function (req, res) {
 
     admins.getAdminDetails(function (errr, result) {
 
         if(!req.param('id'))
         {
-            res.send(result);
+            res.send(200, result);
         }
         else
         {
             var id = req.param('id');
             for (var obj in result) {
                 if(result[obj]._id == id)
-                    res.send(result[obj]);
+                    res.send(200, result[obj]);
             }
         }
     });
 
-})
+});
 
-app.post()
+//save new user
+app.post('/user', function (req, res) {
+    users.addUser(req.body, function (err, result) {
+        if(err) {
+            console.log('err: ',err);
+            res.send(400, err);
+        }
+        else
+        res.send(200, result)
+    })
+});
+
+//save new admin
+app.post('/admin', function (req, res) {
+    admins.addAdmin(req.body, function (err, result) {
+        if(err) {
+            console.log('err: ',err);
+            res.send(400, err);
+        }
+        else
+            res.send(200, result)
+    })
+});
+
 app.listen(8888);
